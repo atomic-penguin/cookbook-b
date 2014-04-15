435a6f611d0c888daf621e9d85eb1533bb2cc4e0
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-04-15 15:58:30 -0700

    disable_setup, add postfix dependency

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures cookbook-a'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'cookbook-a'
+depends          'postfix'
